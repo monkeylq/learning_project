dc110cd6bfdd5308bef8647e1ec2cd069bf72f86
liqi <liqixxoo@qq.com>
2018-12-16 18:50:27 +0800

    asdasdas

---
+++ nettyHello/src/main/java/netty/hello/controller/NettyHelloController.java
@@ -0,0 +1,20 @@
+//package netty.hello.controller;
+//
+//import io.netty.channel.EventLoopGroup;
+//import io.netty.channel.nio.NioEventLoopGroup;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class NettyHelloController
+//{
+//
+//    @GetMapping(value = "nettyHello")
+//    public String hello()
+//    {
+//        EventLoopGroup bossGroup = new NioEventLoopGroup();
+//
+//
+//        return "netty hello";
+//    }
+//}
---
+++ nettyHello/src/main/java/netty/hello/service/nettyDemo.java
@@ -0,0 +1,14 @@
+package netty.hello.service;
+
+public class nettyDemo
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+
+    }
+
+}
